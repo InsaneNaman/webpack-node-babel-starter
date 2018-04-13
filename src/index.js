48787aabf91010c03825f6fbefcb833994192c18
InsaneNaman <namangupta.cse@gmail.com>
2018-04-13 21:03:53 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/*function naman() {
+  setTimeout(() => {
+    console.log("Naman");
+  }, 5000);
+}
+
+naman();
+*/
+
+import restify from 'restify';
+const server = restify.createServer();
+
+server.get('/', (req,res)=>{
+    res.send('Hello World');
+});
+
+server.listen(8080,()=>{
+    console.log(`${server.name} is working at ${server.url}`);
+});
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+ 
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  target: 'node',
+  externals: [
+    {
+        formidable: 'commonjs formidable',
+    },
+]
+};
